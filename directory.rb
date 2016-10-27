4679e6166cad22bb1111c25755343f100780aaae
James <jamesdix@cwgsy.net>
2016-10-27 17:11:51 +0100

    ex7 complete

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
@@ -9,13 +9,17 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november, height: :tall, gender: :male}
+    puts "Please enter #{name}'s cohort"
+    cohort = gets.chomp
+    cohort = 'Unknown'.to_sym if cohort == ""
+    students << {name: name.to_sym, cohort: cohort.to_sym, height: :tall, gender: :male}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
+
   # return the array of students
-  students
+  print students
 end
 
 def print_header
@@ -35,5 +39,5 @@ end
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+#print(students)
 print_footer(students)
