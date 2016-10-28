2c914e5bae9cf5e199746699fd7f95d2a2ea9d3f
James <jamesdix@cwgsy.net>
2016-10-28 17:58:43 +0100

    Saving Data to a File

--- directory.rb
+++ directory.rb
@@ -1,50 +1,52 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the stuents"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into the variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #program terminates
-    else
-      puts "I dont know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit #program terminates
+  else
+    puts "I dont know what you meant, try again"
+  end
+end
 
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  # create an empty array
-  studentsAr = []
   #get the first name
-  name = gets[0..-2]
+  name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    puts "Please enter #{name}'s cohort"
-    cohort = gets.chomp
-    cohort = 'Unknown'.to_sym if cohort == ""
-    studentsAr << {name: name.to_sym, cohort: cohort.to_sym, height: :tall, gender: :male}
-    puts "Now we have #{studentsAr.count} #{plural?(studentsAr.count)}"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} #{plural?(@students.count)}"
     # get another name from the user
     name = gets.chomp
-  end
-  # return the array of students
-  if !studentsAr.empty?
-    return studentsAr
   end
 end
 
@@ -53,27 +55,32 @@ def plural?(n)
 end
 
 
-
 def print_header
   puts "The students of Villains Academy"
   puts "-----------"
 end
 
-def output(students, cohort)
-  studentsInCohort = []
-  if !students
-    return
-  else
-    students.each do |student|
-      studentsInCohort << student[:name] if student[:cohort] == cohort.to_sym
-    end
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-  puts studentsInCohort
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 #nothing happens until we call the methods
+
+def save_students
+  #open the file to writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 interactive_menu
